dbbd8240e652e39aeaf92e80afd0ff17e8d7d338
Sean Murphy <sean@iamseanmurphy.com>
2026-07-15 15:46:11 +0000

    2.6.4

--- axeptio-sdk-integration/vendor/composer/installed.php
+++ axeptio-sdk-integration/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'axeptio/axeptio-wordpress-plugin',
-        'pretty_version' => '2.6.3',
-        'version' => '2.6.3.0',
-        'reference' => 'c68fcfc18e8c435d0ce898639a2d92575b92f6c3',
+        'pretty_version' => '2.6.4',
+        'version' => '2.6.4.0',
+        'reference' => 'cef064e78786a40b219e620bd79c2f7b6f0aea51',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'axeptio/axeptio-wordpress-plugin' => array(
-            'pretty_version' => '2.6.3',
-            'version' => '2.6.3.0',
-            'reference' => 'c68fcfc18e8c435d0ce898639a2d92575b92f6c3',
+            'pretty_version' => '2.6.4',
+            'version' => '2.6.4.0',
+            'reference' => 'cef064e78786a40b219e620bd79c2f7b6f0aea51',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
